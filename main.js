2008230a5ed065f7c148d8417b6e4d8e5d83596c
xamse47u-lab <xamse47u@gmail.com>
2026-07-13 09:07:48 +0300

    Exercise 9 js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 let car = {
-    made : 200,
-    model : "toyota",
-    year : 2026
-}
+  made: 200,
+  model: "toyota",
+  year: 2026,
+  start: function () {
+    console.log("The car has started " + this.model);
+  },
+};
 
 console.log(car);
-
-
+car.start();
 
 console.log(car.made);
 console.log(car.model);
