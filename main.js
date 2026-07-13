52123f6befe21dbc54b25a4d80e894db2eb2c26a
xamse47u-lab <xamse47u@gmail.com>
2026-07-13 07:12:50 +0300

    exercise 7 js

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 let colors = ["blue","Green","yellow"]
+
+console.log("before");
 console.log(colors);
-console.log(colors[0]);
-console.log(colors[1]);
-console.log(colors[2]);
+
+colors.push("red")
+console.log("after push");
+console.log(colors);
+
+
+colors.pop();
+console.log("after pop");
+console.log(colors);
+
+colors.unshift("joseph");
+console.log("after unshift");
+console.log(colors);
+
+
+colors.shift(); 
+console.log("after shift");
+console.log(colors);
+
+console.log(colors.length);
+
 
