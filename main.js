2c8094d21578b93ccf0131cf8749e078f7c06d69
xamse47u-lab <xamse47u@gmail.com>
2026-07-14 08:45:27 +0300

    "exercise 11 js dugsiiye"

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-let library = [
-  {
-    title: "Atomic habits",
-    author: "James Clear",
-    year: 1984,
-  },
-
-  {
-    title: "Mircle morning",
-    author: "Hamse",
-    year: 2026
-  }
-];
-
-console.log(library);
-console.log(library[0].title);
-console.log(library[1].author);
-
+for (let y = 1; 1 < 50; y++) {
+  console.log(y);
+}
