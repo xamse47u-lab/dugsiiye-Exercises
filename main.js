082a0285e57369abf322ed664cfae5803e0c6f81
xamse47u-lab <xamse47u@gmail.com>
2026-07-15 09:47:03 +0300

    exercise 17 js dugsiiye

--- main.js
+++ main.js
@@ -25,9 +25,21 @@
 //   console.log("---------");
 // }
 
-let score = 100;
-if (score > 50) {
-  console.log("passed");
+// let score = 100;
+// if (score > 50) {
+//   console.log("passed");
+// } else {
+//   console.log("failed");
+// }
+
+let temperature = 15;
+
+if (temperature < 0) {
+  console.log("very cold");
+} else if (temperature >= 0 && temperature < 15) {
+  console.log("cold");
+} else if (temperature >= 15 && temperature <= 25) {
+  console.log("warm");
 } else {
-  console.log("failed");
+  console.log("hot");
 }
