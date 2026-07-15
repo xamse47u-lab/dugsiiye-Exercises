a85e2661a3ca9ad6a4d8be6bf1498cc691420589
xamse47u-lab <xamse47u@gmail.com>
2026-07-15 08:38:36 +0300

    exercise 16 js

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
-const people = [
-  {
-    Name: "ahmed",
-    Age: 23,
-    Id: 231,
-  },
-  {
-    Name: "yiius",
-    Age: 37,
-    Id: 221,
-  },
-  {
-    Name: "tear",
-    Age: 73,
-    Id: 423,
-  },
-];
-console.log("\inProperties and values an each person");
+// const people = [
+//   {
+//     Name: "ahmed",
+//     Age: 23,
+//     Id: 231,
+//   },
+//   {
+//     Name: "yiius",
+//     Age: 37,
+//     Id: 221,
+//   },
+//   {
+//     Name: "tear",
+//     Age: 73,
+//     Id: 423,
+//   },
+// ];
+// console.log("\inProperties and values an each person");
 
-for (let person of people) {
-  for (let key in person) {
-    console.log(key + " :" + person[key]);
-  }
+// for (let person of people) {
+//   for (let key in person) {
+//     console.log(key + " :" + person[key]);
+//   }
 
-  console.log("---------");
+//   console.log("---------");
+// }
+
+let score = 100;
+if (score > 50) {
+  console.log("passed");
+} else {
+  console.log("failed");
 }
