a1aa0c6fbb947850b6208c93cf01631cac759766
xamse47u-lab <xamse47u@gmail.com>
2026-07-19 08:51:44 +0300

    exercise 27 js dugsiiye

--- main.js
+++ main.js
@@ -11,24 +11,45 @@
 // console.log(multiplyNumbers(20, 54, 71, 2));
 // console.log(multiplyNumbers(71, 2));
 
-function sooCeli() {
-  alert("war ilaa aad i dhaafto ma tagaysid");
+// function sooCeli() {
+//   alert("war ilaa aad i dhaafto ma tagaysid");
 
-  return { id: 12, name: "Eng Hamse" };
+//   return { id: 12, name: "Eng Hamse" };
+// }
+
+// const user = sooCeli();
+
+// console.log(user);
+
+// console.log("waa i kan aniga waan soo dhaqsaday");
+
+// function nonBlocking() {
+//   setTimeout(() => {
+//     console.log("waad soo daahday nio maxaa kaa qaldan");
+//   }, 2000);
+// }
+
+// console.log("hada ayaan bilabmanaysaa hawsha");
+
+// nonBlocking();
+
+function fetchUserData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let speedTrue = true;
+      if (speedTrue) {
+        resolve("war waan fiicanahay seetahay adiga");
+      } else {
+        reject("war waan xanuunsanahay");
+      }
+    }, 2000);
+  });
 }
 
-const user = sooCeli();
-
-console.log(user);
-
-console.log("waa i kan aniga waan soo dhaqsaday");
-
-function nonBlocking() {
-  setTimeout(() => {
-    console.log("waad soo daahday nio maxaa kaa qaldan");
-  }, 2000);
-}
-
-console.log("hada ayaan bilabmanaysaa hawsha");
-
-nonBlocking();
+fetchUserData()
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
