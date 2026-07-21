1b90f6b8fd5fab4263c8f93e658e74cfa38420c4
xamse47u-lab <xamse47u@gmail.com>
2026-07-21 09:07:25 +0300

    exercise 34 js dugsiiye

--- main.js
+++ main.js
@@ -7,10 +7,26 @@
 // const buttons = document.querySelectorAll(".button");
 // console.log(buttons);
 
-function ChangeContent() {
-  paragraph.textContent = "Welcome my first DOM";
-  //   console.log("welcome our website");
+// function ChangeContent() {
+//   paragraph.textContent = "Welcome my first DOM";
+//   //   console.log("welcome our website");
+// }
+// function ChangeText() {
+//   heading.innerHTML = "Hi welcome our website DOM document";
+// }
+const list = document.querySelector("#list");
+
+function addItem() {
+  const newItem = document.createElement("li");
+  newItem.textContent = "item 4";
+
+  list.appendChild(newItem);
 }
-function ChangeText() {
-  heading.innerHTML = "Hi welcome our website DOM document";
+
+function removeItem() {
+  if (list.lastChild) {
+    list.removeChild(list.lastChild);
+  } else {
+    alert("war waa lagu kala baxay");
+  }
 }
