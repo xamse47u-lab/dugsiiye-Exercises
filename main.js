a4fd6e763e8c250aedc2e56094300d8655e7fffb
xamse47u-lab <xamse47u@gmail.com>
2026-07-21 07:50:27 +0300

    exerscise 33 js dugsiiye

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const heading = document.querySelector("#heading1");
-console.log(heading);
+// const heading = document.querySelector("#heading1");
+// console.log(heading);
 
-const paragraph = document.querySelector("#para");
-console.log(paragraph);
+// const paragraph = document.querySelector("#para");
+// console.log(paragraph);
 
-const buttons = document.querySelectorAll(".button");
-console.log(buttons);
+// const buttons = document.querySelectorAll(".button");
+// console.log(buttons);
+
+function ChangeContent() {
+  paragraph.textContent = "Welcome my first DOM";
+  //   console.log("welcome our website");
+}
+function ChangeText() {
+  heading.innerHTML = "Hi welcome our website DOM document";
+}
